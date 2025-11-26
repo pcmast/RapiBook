20486c1efea6db2f8ed1ccdfc10e2b43c5b67bb0
Pedro <pedroneo2005@gmail.com>
2025-11-26 10:21:44 +0100

    Extructura proyectito

--- src/main/java/com/actividades/rapibookgit/model/Editorial.java
+++ src/main/java/com/actividades/rapibookgit/model/Editorial.java
@@ -1,2 +1,27 @@
+package com.actividades.rapibookgit.model;
+
 public class Editorial {
+    private int id;
+    private String nombre;
+
+    public Editorial(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
